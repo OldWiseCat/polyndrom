38ea52bb41ed05bcaa3297a308b5a07c9c3417a3
nood1 <dummy@mail.com>
2013-07-11 21:26:08 +0400

    acsvav

--- src/Polyndrom.java
+++ src/Polyndrom.java
@@ -6,4 +6,12 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Polyndrom {
+    private static void FindPal(Integer val,Object left,Object right)
+    {
+        for(int i=val;i<999;i++)
+            for(int j=i,j<999;j++)
+        {
+
+        }
+    }
 }
