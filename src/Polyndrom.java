e8634c91091816183624a61aef9ba2968285b37e
Artem Kuraev <artem.kuraev@gmail.com>
2013-07-11 21:50:56 +0400

    очко вообще1

--- src/Polyndrom.java
+++ src/Polyndrom.java
@@ -6,6 +6,10 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Polyndrom {
+    public static boolean isPalindrome(int number) {
+        return true;
+    }
+
     private static void FindPal(Integer val,Object left,Object right)
     {
         for(int i=val;i<999;i++)
@@ -23,14 +27,11 @@ public class Polyndrom {
         }
     }
 
-    public static boolean isPalindrome(int number) {
-        char[] strNumber = Integer.toString(number).toCharArray();
-
-        for (int i = 0, j = strNumber.length - 1; i < j; i++, j--)
-            if (strNumber[i] != strNumber[j])
-                return false;
-        return true;
+    public static void main (String[] args) {
+        int max = 999;
+        while (!isPalindrome(max) && max >= 100) {
+            max--;
+        }
+        //очко2
     }
-
-
 }
