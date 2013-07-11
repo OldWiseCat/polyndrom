b05b7be0f3b6caf3849a86246225bed72cf3d145
Artem Kuraev <artem.kuraev@gmail.com>
2013-07-11 21:18:43 +0400

    initia

---
+++ src/Polyndrom.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: art
+ * Date: 11.07.13
+ * Time: 21:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Polyndrom {
+}
